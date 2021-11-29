e2f74d1cbdabd83e2f70f5f04beb5bd3f5bd20ae
Kalle Fagerberg <kalle.fagerberg@iver.se>
2021-11-29 16:29:29 +0100

    Added background snow video

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,15 +11,36 @@
 	</main>
 </div>
 
-<footer>
-</footer>
+<div class="snow">
+	<video autoplay muted loop>
+		<source src="/videos/Seamless+Snow+Background+Loop-CiXWHUpoD44.mp4"/>
+	</video>
+</div>
 
 <style>
 	.container {
 		margin: 0 auto;
 		max-width: 50rem;
 	}
+
 	main {
 		margin: 1rem;
+	}
+
+	.snow {
+		position: fixed;
+		top: 0;
+		z-index: -100;
+		background-color: #160030;
+		background: linear-gradient(
+			65deg,
+			#160030 0%,
+			#2c1141 100%
+		);
+	}
+	.snow video {
+		min-width: 100vw;
+		min-height: 100vh;
+		mix-blend-mode: screen;
 	}
 </style>
