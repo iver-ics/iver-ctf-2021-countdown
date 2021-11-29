64ed242aebe7a8e1f37cd508ce358c1f637143c3
Kalle Fagerberg <kalle.fagerberg@iver.se>
2021-11-29 15:37:15 +0100

    Fix for GitHub pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,8 +12,12 @@ const config = {
 	kit: {
 		adapter: adapter(),
 
+		paths: {
+			base: dev ? '' : '/iver-ctf-2021-countdown',
+		},
+
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
 	}
 };
 
