c0de4114f9680af9a0fd904af30a820192224903
Kalle Fagerberg <kalle.fagerberg@iver.se>
2021-11-29 15:46:30 +0100

    It's at root

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -11,10 +9,6 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
-		paths: {
-			base: dev ? '' : '/iver-ctf-2021-countdown',
-		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
